b3d6a1b9ca54be7f42f42779ae9580773cb5965d
yusoffnike01 <ronaldoyusoff@gmail.com>
2023-05-23 12:01:08 +0800

    add animate for part screen login

--- styles.js
+++ styles.js
@@ -1,11 +1,10 @@
 import { StyleSheet, Dimensions } from "react-native";
-
-const {width, height} = Dimensions.get('window');
+const { width, height } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'flex-end'
+        justifyContent: 'flex-end',
     },
     button: {
         backgroundColor: 'rgba(123,104,238,0.8)',
@@ -17,28 +16,27 @@ const styles = StyleSheet.create({
         marginVertical: 10,
         borderWidth: 1,
         borderColor: 'white'
-
     },
     buttonText: {
-        fontWeight: '600',
         fontSize: 20,
+        fontWeight: '600',
         color: 'white',
         letterSpacing: 0.5
     },
-    buttomContainer: {
+    bottomContainer: {
         justifyContent: 'center',
-        height: height/3
+        height: height / 3,
     },
-    textinput: {
+    textInput: {
         height: 50,
-        borderRadius: 25,
         borderWidth: 1,
-        borderColor: 'rgba(0,0,0,0.2)',
+        borderColor: 'rgba(0, 0, 0, 0.2)',
         marginHorizontal: 20,
         marginVertical: 10,
+        borderRadius: 25,
         paddingLeft: 10
     },
-    formBottom: {
+    formButton: {
         backgroundColor: 'rgba(123,104,238,0.8)',
         height: 55,
         alignItems: 'center',
@@ -48,36 +46,39 @@ const styles = StyleSheet.create({
         marginVertical: 10,
         borderWidth: 1,
         borderColor: 'white',
-        shadowColor: '#000',
+        shadowColor: "#000",
         shadowOffset: {
             width: 0,
-            height: 7
+            height: 4,
         },
         shadowOpacity: 0.25,
         shadowRadius: 3.84,
-        elevation: 5
+        elevation: 5,
     },
-    formInputContainer:{
-        marginBottom: 70
+    formInputContainer: {
+        marginBottom: 70,
+        ...StyleSheet.absoluteFill,
+        zIndex: -1,
+        justifyContent: 'center',
     },
     closeButtonContainer: {
         height: 40,
         width: 40,
         justifyContent: 'center',
         alignSelf: 'center',
-        shadowColor: '#000',
+        shadowColor: "#000",
         shadowOffset: {
             width: 0,
-            height: 5
+            height: 5,
         },
         shadowOpacity: 0.34,
         shadowRadius: 6.27,
         elevation: 1,
         backgroundColor: 'white',
         alignItems: 'center',
-        borderRadius: 20
+        borderRadius: 20,
+        top: -20
     }
-
 });
 
 export default styles;
